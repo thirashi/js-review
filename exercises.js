823e6d6530dbfcb5a213e8c187a62295b713d72f
Tyler Hirashima <tylerhirashima@Tylers-MacBook-Pro-5.local>
2018-02-28 19:50:05 -1000

    Finished Ex5

--- exercises.js
+++ exercises.js
@@ -43,11 +43,23 @@ console.log(drinksGin);
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
 
+var completedCodingChallenge = null;
+console.log(completedCodingChallenge);
+
+var traveledToSpace = null;
+console.log(traveledToSpace);
+
+
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of any 5 prefectures in Japan.
 Console log the variable 
 Console log the value at index 1.
 Console log the value at index 3.*/
+
+var japanPrefectures = ["Niigata", "Tokyo", "Nagano", "Fukuoka", "Okinawa"];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
 
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
